8fe48749a5fe279b6db444fdf074f9bc8614e3b0
HelloImDevion <devionfothergill@gmail.com>
2017-11-08 17:01:47 -0500

    first commit

---
+++ Week13WardrobeMVC/Models/Week13WardrobeMVCContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Week13WardrobeMVC.Models
+{
+    public class Week13WardrobeMVCContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Week13WardrobeMVCContext() : base("name=Week13WardrobeMVCContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Week13WardrobeMVC.Models.Top> Tops { get; set; }
+
+        public System.Data.Entity.DbSet<Week13WardrobeMVC.Models.Bottom> Bottoms { get; set; }
+
+        public System.Data.Entity.DbSet<Week13WardrobeMVC.Models.Accessory> Accessories { get; set; }
+
+        public System.Data.Entity.DbSet<Week13WardrobeMVC.Models.Sho> Shoes { get; set; }
+    }
+}
---
+++ Week13WardrobeMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week13WardrobeMVC.Startup))]
+namespace Week13WardrobeMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
